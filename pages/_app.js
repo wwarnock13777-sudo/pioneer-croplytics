eb6040e6d899b1b18e027a2546172b23bd514c8b
wwarnock13777-sudo <wwarnock13777@gmail.com>
2026-05-27 00:11:19 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="/icon.png"/>
+        <link rel="icon" href="/icon.png"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta name="apple-mobile-web-app-title" content="CropLytics"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
